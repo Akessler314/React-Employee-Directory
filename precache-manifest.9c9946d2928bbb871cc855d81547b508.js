13107bd6b37ccc8269c4e1ec58c5d0ed9f6964b1
Andrew Kessler <andrew.kessler95@gmail.com>
2020-07-31 12:05:08 -0600

    Updates

---
+++ precache-manifest.9c9946d2928bbb871cc855d81547b508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825e271f18a85a907175cbeadb2afd92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3059108899ac1fff66bd",
+    "url": "/static/css/main.8ba35079.chunk.css"
+  },
+  {
+    "revision": "a7e5a37ea250415e8e64",
+    "url": "/static/js/2.fd47122e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fd47122e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3059108899ac1fff66bd",
+    "url": "/static/js/main.69212d24.chunk.js"
+  },
+  {
+    "revision": "3eaec5ba2d859ccbd2c8",
+    "url": "/static/js/runtime-main.d68be137.js"
+  }
+]);
